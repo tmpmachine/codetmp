5a7dfd8e106acc3d79f93d15c3048ca4867918b2
tmpmachine <bill.yosafatz@gmail.com>
2019-07-23 10:06:05 +0700

    v1.00, UI update, remove garbage code

--- tmpmachine/sw.js
+++ tmpmachine/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 });
 
 
-let cacheVersion = '0.036';
+let cacheVersion = '1.00';
 let cacheItem = 'tmp-'+cacheVersion;
 
 self.addEventListener('install', function(event) {
