0924320d1b03fa9d9e184033c42891058106ff54
tmpmachine <bill.yosafatz@gmail.com>
2019-08-07 06:33:27 +0700

    deploy v1.061

--- tmpmachine/sw.js
+++ tmpmachine/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 });
 
 
-let cacheVersion = '1.016';
+let cacheVersion = '1.0161';
 let cacheItem = 'tmp-'+cacheVersion;
 
 self.addEventListener('install', function(event) {
