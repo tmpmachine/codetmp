6d03d66a6ee35882b0527c1dc78df722090d22c6
tmpmachine <bill.yosafatz@gmail.com>
2019-12-02 11:06:17 +0700

    update aww.js

--- tmpmachine/require/aww.js
+++ tmpmachine/require/aww.js
@@ -1,5 +1,5 @@
 /*
-0.053 : 28 -- fix notification blocking
+0.054 : 2 dec -- fix aww element initialization, fix pop out animation
 */
 
 (function () {
@@ -10,7 +10,7 @@
     
     let popElement = document.createElement('div');
     
-    (function () {
+    function initPopElement() {
       popElement.setAttribute('style','height:0;overflow:hidden;position:fixed;top:0;z-index:1000;width:100%;text-align:center;');
       popElement.innerHTML = "<div style='-webkit-transform:translateY(-150%);transform:translateY(-150%);-webkit-transition:-webkit-transform 500ms;transition:transform 500ms;margin-top:4px;box-shadow: 0 0px 10px 0 rgba(0,0,0,0.2), 0 0px 5px 0 rgba(0,0,0,0.19);background:white;border-radius:2px;display:inline-block;padding:4px 8px;font-family:sans-serif;font-size:0.8rem;color:#000!important;'>&nbps;</div>";
       document.body.appendChild(popElement);
@@ -18,10 +18,15 @@
       window.onresize = function() {
         popElement.style.left = (document.body.clientWidth - popElement.offsetWidth) / 2 + 'px';
       };
-    })();
+    }
+    
+    initPopElement();
     
     this.pop = function (content, isPersistence = false, timeout = 2000) {
       
+      if (popElement.parentNode === null)
+        initPopElement();
+        
       popElement.firstElementChild.innerHTML = content;
       popElement.style.left = (document.body.clientWidth - popElement.offsetWidth) / 2 + 'px';
       popElement.firstElementChild.style.webkitTransform = 'translateY(0px)';
@@ -29,10 +34,12 @@
       popElement.style.overflow = 'visible';
       
       clearTimeout(this.hide);
-      this.hide = setTimeout(function () {
+      this.hide = setTimeout(function() {
         popElement.firstElementChild.style.webkitTransform = 'translateY(-150%)';
         popElement.firstElementChild.style.transform = 'translateY(-150%)';
-        popElement.style.overflow = 'hidden';
+        setTimeout(function() {
+          popElement.style.overflow = 'hidden';
+        }, 250);
       }, timeout);
       
       if (isPersistence)
--- tmpmachine/sw.js
+++ tmpmachine/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 });
 
 
-let cacheVersion = '1.06';
+let cacheVersion = '1.061';
 let cacheItem = 'tmp-'+cacheVersion;
 
 self.addEventListener('install', function(event) {
