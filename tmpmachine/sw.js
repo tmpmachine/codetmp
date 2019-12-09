3883bde8f965e725dd2373c01deef8b95b652055
tmpmachine <tmpmachine7@gmail.com>
2019-12-09 15:31:49 +0700

    deploy v1.064

--- tmpmachine/sw.js
+++ tmpmachine/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 });
 
 
-let cacheVersion = '1.0631';
+let cacheVersion = '1.064';
 let cacheItem = 'tmp-'+cacheVersion;
 
 self.addEventListener('install', function(event) {
