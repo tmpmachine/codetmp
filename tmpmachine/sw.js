b295dc6ac027e58462e4596e21190cc21034cdab
tmpmachine <tmpmachine@gmail.com>
2020-03-17 23:16:05 +0700

    deploy v1.0724

--- tmpmachine/sw.js
+++ tmpmachine/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 });
 
 
-let cacheVersion = '1.0723';
+let cacheVersion = '1.0724';
 let cacheItem = 'tmp-'+cacheVersion;
 
 self.addEventListener('install', function(event) {
