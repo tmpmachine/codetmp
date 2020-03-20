09a656bf9591068c5bed623245f6c6d320970f53
tmpmachine <tmpmachine@gmail.com>
2020-03-20 11:56:40 +0700

    change idb.js directory

--- attemp/sw.js
+++ attemp/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message',function(e){
 });
 
 
-var cacheVersion = '0.018';
+var cacheVersion = '0.0181';
 var cacheItem = 'tmp'+cacheVersion;
 
 self.addEventListener('install',function(event) {
