d00607ed763b4bee93615b96cf176f95e99f31c6
tmpmachine <tmpmachine@gmail.com>
2020-06-28 09:00:32 +0700

    update ace.js - improved mobile context menu positioning

--- tmpmachine/sw.js
+++ tmpmachine/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
 });
 
 
-let cacheVersion = '1.175';
+let cacheVersion = '1.177';
 let cacheItem = 'tmp-'+cacheVersion;
 
 self.addEventListener('install', function(event) {
