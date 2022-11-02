8a3564885621ed5d60aec4c1c0d5d2e6f037bb44
tmpmachine <bill.yosafatz@gmail.com>
2022-11-03 06:27:45 +0700

    increase version

--- codetmp/sw.js
+++ codetmp/sw.js
@@ -1,5 +1,5 @@
 L = console.log;
-let cacheVersion = '34';
+let cacheVersion = '35';
 let cacheItem = 'codetmp-'+cacheVersion;
 const ACE_CDN_BASEPATH = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6';
 
