c75bc25a57f6e1675565caadc0bf1bcf970d58f6
tmpmachine <bill.yosafatz@gmail.com>
2022-11-21 06:31:50 +0700

    update version

--- codetmp/sw.js
+++ codetmp/sw.js
@@ -1,5 +1,5 @@
 L = console.log;
-let cacheVersion = '38';
+let cacheVersion = '39';
 let cacheItem = 'codetmp-'+cacheVersion;
 const ACE_CDN_BASEPATH = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6';
 
