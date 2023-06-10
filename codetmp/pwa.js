325a712d11e305819c2f535a0dc16474a0155f53
tmpmachine <bill.yosafatz@gmail.com>
2023-06-10 04:19:47 +0000

    update(app cache): allow for manual updates

---
+++ codetmp/pwa.js
@@ -0,0 +1,62 @@
+window.pwa = (function() {
+  
+  let cacheName = 'codetmp-MTE0MDc5NjU';
+
+  let SELF = {};
+  
+  function extractUrlsFromJson(json) {
+    let urls = [];
+    for (let key in json) {
+      if (key == "skip") {
+        continue;
+      }
+      if (Array.isArray(json[key])) {
+        urls = urls.concat(json[key]);
+      }
+    }
+    return urls;
+  }
+    
+  SELF.removeUpdate = function() {
+    
+    caches.delete(cacheName)
+      .then(() => {
+          alert('Done! Reload to take effect.');
+      });
+  };
+  
+  SELF.update = function() {
+    
+    fetch('manifest-cache.json')
+    .then(res => res.json())
+    .then(json => {
+      
+      let cacheURLs = extractUrlsFromJson(json);
+  
+      caches.delete(cacheName)
+      .then(() => {
+        caches.open(cacheName)
+        .then(function(cache) {
+          return Promise.all(
+            cacheURLs.map(function(url) {
+              return cache.add(url).catch(function(error) {
+                console.error('Failed to cache URL:', url, error);
+              });
+            })
+          );
+        })
+        .then(function() {
+          alert('Done! Reload to take effect.');
+        })
+        .catch(function(error) {
+          alert('Failed. Check console.');
+        });
+      });
+    
+    });
+    
+  };
+  
+  return SELF; 
+  
+})();
--- codetmp/sw.js
+++ codetmp/sw.js
@@ -1,7 +1,27 @@
-L = console.log;
-let cacheVersion = '67';
-let cacheItem = 'codetmp-'+cacheVersion;
-const ACE_CDN_BASEPATH = 'https://cdnjs.cloudflare.com/ajax/libs/ace/1.9.6';
+let cacheName = 'codetmp-MTE0MDc5NjU';
+
+// delete old cache versioning
+// use manifest-cache.json for future updates
+caches.keys().then(function(c){
+  c.map(function(cacheName){
+    if (cacheName.split('-')[1].length <= 2) {
+      caches.delete(cacheName);
+    }
+  });
+});
+
+function extractUrlsFromJson(json) {
+  let urls = [];
+  for (let key in json) {
+    if (key == "skip") {
+      continue;
+    }
+    if (Array.isArray(json[key])) {
+      urls = urls.concat(json[key]);
+    }
+  }
+  return urls;
+}
 
 self.addEventListener('message', function(e) {
   if (e.data.action == 'skipWaiting') {
@@ -12,117 +32,43 @@ self.addEventListener('message', function(e) {
 });
 
 self.addEventListener('install', function(event) {
-
-  let urls = [
-    '/',
-    '/manifest.json',
-    '/css/style.css',
-    '/css/app.css',
-    '/css/file-tree.css',
-    
-    '/assets/js/fflate.js',
-    '/assets/js/sha256.js',
-
-    // ace custom theme
-    '/assets/ace/theme-codetmp-markdown.js',
-    '/assets/ace/theme-codetmp.js',
-    'https://cdn.jsdelivr.net/gh/tmpmachine/statelist-utility@v1.0.2/statelist.min.js',
-    
-    `${ACE_CDN_BASEPATH}/ace.js`,
-    `${ACE_CDN_BASEPATH}/theme-github.js`,
-    `${ACE_CDN_BASEPATH}/mode-html.js`,
-    `${ACE_CDN_BASEPATH}/mode-json.js`,
-    `${ACE_CDN_BASEPATH}/mode-css.js`,
-    `${ACE_CDN_BASEPATH}/mode-javascript.js`,
-    `${ACE_CDN_BASEPATH}/worker-html.js`,
-    `${ACE_CDN_BASEPATH}/worker-json.js`,
-    `${ACE_CDN_BASEPATH}/worker-css.js`,
-    `${ACE_CDN_BASEPATH}/worker-javascript.js`,
-    `${ACE_CDN_BASEPATH}/ext-searchbox.js`,
-    `${ACE_CDN_BASEPATH}/ext-prompt.js`,
-    `${ACE_CDN_BASEPATH}/ext-language_tools.js`,
-    `${ACE_CDN_BASEPATH}/snippets/javascript.js`,
-    `${ACE_CDN_BASEPATH}/snippets/html.js`,
-    `${ACE_CDN_BASEPATH}/snippets/markdown.js`,
-    '/assets/icons/folder_open.svg',
-    '/assets/icons/folder_close.svg',
-
-    '/assets/images/128.png',
-    '/assets/images/128ap.png',
-    '/assets/images/192.png', 
-    '/assets/images/GitHub_Logo.png', 
-    
-    'https://fonts.googleapis.com/css2?family=Material+Icons+Round', 
-    'https://fonts.gstatic.com/s/materialiconsround/v37/LDItaoyNOAY6Uewc665JcIzCKsKc_M9flwmP.woff2', 
-
-    'https://cdn.jsdelivr.net/npm/idb@7/build/umd.js',
-
-    '/views/modals.html',
-    '/views/templates.html',
-
-    'https://cdn.jsdelivr.net/gh/tmpmachine/divless-html@v1.0.1/divless.min.js',
-    '/js/require/o.js',
-    '/js/require/lsdb.js',
-    '/js/require/keyboard.js',
-    '/js/require/odin.js',
-    '/js/require/aww.js',
-    '/js/require/jszip.min.js',
-
-    '/js/components/ext-firebase.js',
-    '/js/components/auth2helper.js',
-    '/js/components/support.js',
-    '/js/components/extension.js',
-    '/js/components/preferences.js',
-    '/js/components/modal.js',
-    '/js/components/helper.js',
-    '/js/components/tab-manager.js',
-    '/js/components/file-reader.js',
-    '/js/components/file-bundler.js',
-    '/js/components/git.js',
-    '/js/components/notifier.js',
-    '/js/components/snippet-manager.js',
-    '/js/components/preview-handler.js',
-    '/js/components/clipboard.js',
-    '/js/components/file-manager.js',
-    '/js/components/file-tree.js',
-    '/js/components/drive.js',
-    '/js/components/keyboard-handler.js',
-    '/js/components/defer-feature-1.js',
-    '/js/components/defer-feature-2.js',
-    '/js/components/single-file-generator.js',
-    '/js/components/environment-manager.js',
-    
-    '/js/core/app.js',
-    '/js/core/app-data.js',
-    
-    '/js/dom-events.js',
-    '/js/ux.js',
-    
-    '/index.js',
-  ];
- 
   event.waitUntil(
-    Promise.all([
-      caches.open(cacheItem).then(function(cache) {
-        return cache.addAll(urls);
-      }),
-    	self.skipWaiting(),
-    ]).catch(error => {
-      console.error(error);
-    })
-  );  
+    recache()
+  );
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(Promise.all([
-    caches.keys().then(function(c) {
-      c.map(function(cname) {
-        if (!cname.endsWith(cacheVersion))
-          caches.delete(cname);
+function recache() {
+  return fetch('manifest-cache.json')
+    .then(res => res.json())
+    .then(json => {
+      let cacheURLs = extractUrlsFromJson(json);
+      caches.delete(cacheName)
+      .then(() => {
+        caches.open(cacheName)
+        .then(function(cache) {
+          return Promise.all(
+            cacheURLs.map(function(url) {
+              return cache.add(url).catch(function(error) {
+                console.error('Failed to cache URL:', url, error);
+              });
+            })
+          );
+        })
+        .then(function() {
+          console.log('Files successfully cached.');
+        })
+        .catch(function(error) {
+          console.log(error);
+          console.log('Failed to cache all required files.');
+        });
       });
-    }),
-  	self.clients.claim(),
-  ]));
+    });
+}
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    recache()
+  );
 });
 
 self.addEventListener('fetch', function(e) {
@@ -142,7 +88,7 @@ self.addEventListener('fetch', function(e) {
 
 function cacheExtension(e) {
   	e.waitUntil(Promise.all([
-      caches.open(cacheItem).then(function(cache) {
+      caches.open(cacheName).then(function(cache) {
         return cache.addAll(e.data.files);
       }),
       e.source.postMessage({ 
