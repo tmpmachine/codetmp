910932e076239f5c14b07ee7e76cab570c96d232
tmpmachine <bill.yosafatz@gmail.com>
2024-01-01 06:34:55 +0000

    bump version

--- codetmp/sw.js
+++ codetmp/sw.js
@@ -2,7 +2,7 @@
   increase below number to trigger service worker update/reactivation
   to deliver latest updates for all users automatically on page visit
   
-  unique numer : 10019
+  unique numer : 10020
 */
 
 let cacheName = 'codetmp-MTE0MDc5NjU';
