3bebf661a78e98644285fc41d1dad5c13d5da45d
tmpmachine <bill.yosafatz@gmail.com>
2024-07-31 11:08:15 +0000

    migration fix

--- codetmp/sw.js
+++ codetmp/sw.js
@@ -2,7 +2,7 @@
   increase below number to trigger service worker update/reactivation
   to deliver latest updates for all users automatically on page visit
   
-  unique numer : 10041
+  unique numer : 20047
 */
 
 let cacheName = 'codetmp-MTE0MDc5NjU';
