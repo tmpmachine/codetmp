55927fe36e0dd5ac3e2fbe71afafd979fd7b4d6f
tmpmachine <bill.yosafatz@gmail.com>
2025-01-29 11:16:39 +0700

    feat(env): update default development port

--- codetmp/environment.js
+++ codetmp/environment.js
@@ -1,13 +1,13 @@
 let environment = {
-    activeEnvironment: 'production', // production or development
+    activeEnvironment: 'development', // production or development
     
     production: {
         previewUrl: 'https://preview.codetmp7.dev/',
         previewUrlPWA: 'https://pwa.codetmp7.dev/', 
     }, 
     development: {
-        previewUrl: 'http://localhost:8001/',
-        previewUrlPWA: 'http://localhost:8002/', 
+        previewUrl: 'http://localhost:8202/',
+        previewUrlPWA: 'http://localhost:8202/', 
     }
 };
 
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 
 let servers = [
-  { port: 8000, dir: 'codetmp/', },
-  { port: 8001, dir: 'cpreview/', },
-  { port: 8002, dir: 'cpreview/', }, // alternative, can be used to test serve production PWA
-  { port: 8003, dir: 'deploy/codetmp/', }, // minified build files
+  { port: 8201, dir: 'codetmp/', },
+  { port: 8202, dir: 'cpreview/', },
 ];
 
 // Start servers
